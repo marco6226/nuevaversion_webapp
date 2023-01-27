0ce71c5d35cfbeb16f4b56783a1ec1b9be484559
John Rueda <risgan2727@gmail.com>
2023-01-26 22:43:45 -0500

    Se comenta licencia syncfusion

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { registerLicense } from '@syncfusion/ej2-base';
 
-registerLicense('@32302e342e30AoEO2o9F0VLGRrHSvGmrLxzI4MS+LHub4Y4WHU9/4V8=');
+// registerLicense('@32302e342e30AoEO2o9F0VLGRrHSvGmrLxzI4MS+LHub4Y4WHU9/4V8=');
 
 
 if (environment.production) {
