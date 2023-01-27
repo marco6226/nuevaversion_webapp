a05d9d7df50167b1e620a85e8d1de305cabe5f8f
John Rueda <risgan2727@gmail.com>
2023-01-26 22:44:12 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { registerLicense } from '@syncfusion/ej2-base';
+// import { registerLicense } from '@syncfusion/ej2-base';
 
 // registerLicense('@32302e342e30AoEO2o9F0VLGRrHSvGmrLxzI4MS+LHub4Y4WHU9/4V8=');
 
