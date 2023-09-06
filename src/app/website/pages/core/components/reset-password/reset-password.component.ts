0825916c666ffb12e37399e0bcd8ae71b1118abd
Marco Acero <60655434+marco6226@users.noreply.github.com>
2023-09-06 17:26:25 -0500

    fis toast al recuperar contraseña SIG-541

--- src/app/website/pages/core/components/reset-password/reset-password.component.ts
+++ src/app/website/pages/core/components/reset-password/reset-password.component.ts
@@ -28,27 +28,30 @@ export class ResetPasswordComponent {
 
   async reset(value: any){
     // debugger
-    this.messageService.add({ severity: 'info', detail: 'Ha solicitado restaurar su contraseña, por favor espere ', summary: 'Solicitando...' });
+    this.messageService.add({ severity: 'info', detail: 'Ha solicitado restaurar su contraseña, por favor espere ', summary: 'Solicitando...', key: 'appToast', life: 5000 });
     
     let email = value.correo;
         if (email == null || email == '') {
-            this.messageService.add({ severity: 'warn', summary: 'Correo electrónico requerido', detail: 'Debe especificar el correo electrónico de la cuenta de usuario' });
+            this.messageService.add({ severity: 'warn', summary: 'Correo electrónico requerido', detail: 'Debe especificar el correo electrónico de la cuenta de usuario', key: 'appToast', life: 5000 });
             return;
         }
         this.visibleLnkResetPasswd = false;
-        await this.authService.resetPasswd(email).then(
+        this.authService.resetPasswd(email).then(
             (resp: any) => {
-                this.messageService.add({ severity: resp['tipoMensaje'], detail: resp['detalle'], summary: resp['mensaje'] });
-                this.IsVisible = true
-                setTimeout(() => {
-                  this.router.navigate(['login']); 
-                  this.IsVisible = false
-
-                }, 1200);
+                this.messageService.add({ severity: resp['tipoMensaje'], detail: resp['detalle'], summary: resp['mensaje'], key: 'appToast', life: 5000});
+                this.IsVisible = true;
+                this.router.navigate(['login']); 
+                this.IsVisible = false;
             }
         ).catch((err: any) => {
-            this.messageService.add({ severity: err.error['tipoMensaje'], detail: err.error['detalle'], summary: err.error['mensaje'] });
+          console.log(typeof err, err);
+          if(err.status === 0){
+            this.messageService.add({ severity: 'error', detail: 'No se pudo establecer conexión con el servidor', summary: 'Error de conexión', key: 'appToast', life: 5000});
             this.visibleLnkResetPasswd = true;
+          } else {
+            this.messageService.add({ severity: err.error['tipoMensaje'], detail: err.error['detalle'], summary: err.error['mensaje'], key: 'appToast', life: 5000 });
+            this.visibleLnkResetPasswd = true;
+          }
         });
   }
 
