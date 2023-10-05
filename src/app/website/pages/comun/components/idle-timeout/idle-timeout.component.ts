c6f2043b559e07bad983b6bd5afcc5b35d98af70
Juanse-92 <juanbernal@lerprevencion.com>
2023-10-05 16:41:14 -0500

    timeout- session

--- src/app/website/pages/comun/components/idle-timeout/idle-timeout.component.ts
+++ src/app/website/pages/comun/components/idle-timeout/idle-timeout.component.ts
@@ -15,7 +15,7 @@ export class IdleTimeoutComponent implements OnInit {
   timeoutID: any;
   intervalID: any;
   timeOut = 600_000;
-  // timeOut = 6_000;
+  // timeOut = 10_000;
 
   countDown = 0;
 
@@ -28,12 +28,14 @@ export class IdleTimeoutComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    
     window.addEventListener('mousemove', this.resetTimer.bind(this));
     window.addEventListener('keypress', this.resetTimer.bind(this));
     this.startTimer();
   }
 
   resetTimer() {
+    
     if (!this.visibleBtn) {
       window.clearTimeout(this.timeoutID);
       this.goActive();
@@ -41,37 +43,61 @@ export class IdleTimeoutComponent implements OnInit {
   }
 
   startTimer() {
+    
+    localStorage.setItem('mouseMove','true')
+    let hora=new Date()
+    localStorage.setItem('time',hora.toString())
     this.timeoutID = window.setTimeout(() => { this.goInactive() }, this.timeOut);
   }
 
   goInactive() {
-    this.expired = true;
+
+    localStorage.setItem('mouseMove','false')
+
     // this.intervalID = setInterval(() => this.countInterval(), 10);
     this.intervalID = setInterval(() => this.countInterval(), 300);
   }
 
   async countInterval() {
-    this.countDown = this.countDown + 1;
-    // this.divBar['nativeElement'].style.width = this.countDown + '%';
     
-    if (this.countDown >= 100) {
+    let mouseMove:any
+    mouseMove=localStorage.getItem('mouseMove')
+
+    let hora: string=localStorage.getItem('time')!
+    let horaD=(new Date()).getTime()-new Date(hora).getTime()
+
+    if(mouseMove=='true'){
+      this.resetTimer()
+      this.startTimer()
+      this.goActive()
+    }else if(mouseMove=='false' && horaD>this.timeOut){
+      this.expired = true;
+      this.countDown = this.countDown + 1;
+      // this.divBar['nativeElement'].style.width = this.countDown + '%';
       
-      this.countDown = 100;
-      this.visibleBtn = true;
+      if (this.countDown >= 100) {
+        
+        this.countDown = 100;
+        this.visibleBtn = true;
 
-      clearInterval(this.intervalID);
-      await this.authService.logout().then(
-        async (resp: any) => {
-          // console.log(resp)
+        clearInterval(this.intervalID);
+        await this.authService.logout().then(
+          async (resp: any) => {
 
-          this.visibleBtn = true;
-        }
-      );
+            this.visibleBtn = true;
+          }
+        );
+        localStorage.setItem('mouseMove','0')
+        await this.redireccionar()
+      }
+    }else if(mouseMove=='0'){
+    // else{
       await this.redireccionar()
     }
   }
 
   goActive() {
+    
     if (this.expired == true) {
       this.expired = false;
       clearInterval(this.intervalID);
@@ -81,7 +107,11 @@ export class IdleTimeoutComponent implements OnInit {
   }
 
   async redireccionar(){
+    
     await this.navService.redirect('/login');
+    setTimeout(() => {
+      localStorage.clear()
+    }, 2000);
   }
 
 }
