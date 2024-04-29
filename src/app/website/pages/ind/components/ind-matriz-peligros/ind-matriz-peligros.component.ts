59c3245f1e30d3cc7562afaa67d32a81216ce96c
Juanse-92 <juanbernal@lerprevencion.com>
2024-04-29 14:42:18 -0500

    fase dos salud laboral sebas to fredy

---
+++ src/app/website/pages/ind/components/ind-matriz-peligros/ind-matriz-peligros.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ind-matriz-peligros',
+  templateUrl: './ind-matriz-peligros.component.html',
+  styleUrl: './ind-matriz-peligros.component.scss'
+})
+export class IndMatrizPeligrosComponent implements OnInit{
+  constructor(
+ 
+  ) {
+ 
+
+  }
+  async ngOnInit(): Promise<void> {}
+}
